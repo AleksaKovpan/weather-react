030509fbd21acad3d1efcab786f09ba19154a209
AleksaKovpan <115225761+AleksaKovpan@users.noreply.github.com>
2022-12-20 11:25:47 +0200

    Added loader Audio

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Audio } from "react-loader-spinner";
 export default function Weather() {
   function handleResponse(response) {
     alert(`The weather in Kyiv is ${Math.round(response.data.main.temp)}°C`);
@@ -7,5 +8,15 @@ export default function Weather() {
   let apiKey = "9219390dd657b91d9e5236012585e598";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return <div>Weather</div>;
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="three-dots-loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
 }
