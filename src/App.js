3c3dd1430a176537e0802e90f42d7c461f6bdcb3
AleksaKovpan <115225761+AleksaKovpan@users.noreply.github.com>
2022-12-20 13:00:01 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
